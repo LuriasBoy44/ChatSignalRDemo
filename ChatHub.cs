a72d6fd1f55f8997c1aee1b2ed2eb66b4951aab6
LuriasBoy44 <claudio.ruiz44@hotmail.com>
2021-11-13 15:06:24 -0300

    versióm 1.0

---
+++ ChatHub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatRoomsProyect
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string room, string message)
+        {
+            await Clients.Group(room).SendAsync("ReceiveMessage",user,message);
+        
+        }
+        public async Task AddToGroup( string room)
+        {
+
+            await Groups.AddToGroupAsync(Context.ConnectionId,room);
+            await Clients.Group(room).SendAsync("ShowWho", $"Algien se conectó {Context.ConnectionId}");
+
+        }
+    }
+}
---
+++ Controllers/ChatController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ChatRoomsProyect.Models;
+
+namespace ChatRoomsProyect.Controllers
+{
+    public class ChatController : Controller
+    {
+        public static Dictionary<int, String> Rooms = new Dictionary<int, string>() {
+
+            { 1,"Sala 1"},
+            { 2,"Sala Dos"},
+            { 3,"Sala III"},
+            { 4,"sAlA kUaTr0"},
+            { 5,"Salon 5" }
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Room(int room)
+        {
+            return View("Room", room);
+        }
+    }
+}
